a0f2eac039801c98dccbaebc91a1daee773f1fc3
Genesis Sarabia <genesissarabia14@gmail.com>
2023-06-19 11:32:42 -0500

    first commit, adding gitignore

---
+++ src/main/java/com/yessyscreations/yessyscreations/YessysCreationsApplication.java
@@ -0,0 +1,13 @@
+package com.yessyscreations.yessyscreations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YessysCreationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YessysCreationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yessyscreations/yessyscreations/YessysCreationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yessyscreations.yessyscreations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YessysCreationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
