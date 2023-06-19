b28752d5097755ca33f69c729219e6751e699269
Genesis Sarabia <genesissarabia14@gmail.com>
2023-06-19 17:27:18 -0500

    added userController and added thymeleaf dependency to pom

--- src/main/java/com/yessyscreations/yessyscreations/controllers/UserController.java
+++ src/main/java/com/yessyscreations/yessyscreations/controllers/UserController.java
@@ -1,4 +1,24 @@
 package com.yessyscreations.yessyscreations.controllers;
 
+import com.yessyscreations.yessyscreations.repositories.CraftRepository;
+import com.yessyscreations.yessyscreations.repositories.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    private UserRepository userDao;
+    private CraftRepository craftDao;
+
+    public UserController(UserRepository userDao, CraftRepository craftDao){
+        this.userDao = userDao;
+        this.craftDao = craftDao;
+    }
+
+    @GetMapping("/")
+    public String homePage(){
+        return "index";
+    }
 }
